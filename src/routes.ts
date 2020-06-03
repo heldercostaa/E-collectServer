bd8bdd7ee8638442ebb73f4af61d30cea0f6f2b5
Helder Costa <helderlpcosta@gmail.com>
2020-06-03 07:25:32 -0300

    Add route to add a simple version of points

--- src/routes.ts
+++ src/routes.ts
@@ -9,12 +9,39 @@ routes.get('/items', async (req, res) => {
   
   const serializedItems = items.map(item => {
     return {
+      id: item.id,
       title: item.title,
       image_url: `http://localhost:3333/uploads/${item.image}`,
     };
   });
 
   return res.json(serializedItems);
+});
+
+routes.post('/points', async (req, res) => {
+  const {
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+    items
+  } = req.body;
+
+  await knex('points').insert({
+    image: 'image-fake',
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf
+  });
+
+  return res.json({ success: true });
 });
 
 export default routes;
