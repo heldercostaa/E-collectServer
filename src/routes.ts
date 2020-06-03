308d53a31d49854e18e1067f42762c55f97eb827
Helder Costa <helderlpcosta@gmail.com>
2020-06-03 08:53:35 -0300

    Add route to get filtered points

--- src/routes.ts
+++ src/routes.ts
@@ -10,6 +10,7 @@ const routes = express.Router();
 routes.get('/items', itemsController.index);
 
 routes.post('/points', pointsController.create);
+routes.get('/points', pointsController.index);
 routes.get('/points/:id', pointsController.show);
 
 export default routes;
