82b21c2fcddab0b686d91d83db0c3397161740a2
Helder Costa <helderlpcosta@gmail.com>
2020-06-03 07:18:20 -0300

    Add route to list all items

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,20 @@
 
 import express from 'express';
+import knex from './database/connection'
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-  return res.json({message: 'Hello'})
+routes.get('/items', async (req, res) => {
+  const items = await knex('items').select('*');
+  
+  const serializedItems = items.map(item => {
+    return {
+      title: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`,
+    };
+  });
+
+  return res.json(serializedItems);
 });
 
 export default routes;
